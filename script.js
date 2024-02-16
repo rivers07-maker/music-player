36ed25f479a18e5656bfc3e55fe5e9cef91f3ac0
rivers07-maker <riosandres91@gmail.com>
2024-02-16 11:10:42 -0400

    Music player with javascript

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
+// regresa un dato async promise
 axios.get('https://leonardoapi.onrender.com/songs')
+
+  // cuando se cumpla promise se ejecuta el then
+  // cada funcion tipo flecha es un callback
   .then((res) => {
-    console.log(res.data)
+
+
+    // recorrer la lista de canciones con el metodo map
+    // cada song es una cancion distinta en cada vuelta del bucle .map
     res.data.songs.map((song) => {
 
-      console.log(song)
-
+      // comienza la linea de ensamblaje
+      // creamos el elemento
       let li = document.createElement('li')
 
+      // Metemos el contenido
+      // el contenido es dinamico
       li.innerHTML = `
-
           <img src="${song.path.front}">
           <h2>${song.title}</h2>
           <p>${song.author}</p>
 
       `
+
+      // damos funcionalidad al elemento
       li.addEventListener('click', () => {
 
+        // aqui adentro cambiamos la path del audio
         document.getElementById('current-song-audio').setAttribute('src', song.path.audio)
+        document.getElementById('current-song-img').setAttribute('src', song.path.front)
+        document.getElementById('current-song-title').innerHTML = song.title
+
+        const sonando = document.getElementsByClassName('sonando')
+        if (sonando.length > 0) {
+          sonando[0].classList.remove('sonando')
+        }
+
+        li.classList.add('sonando')
       })
 
+      // metemos el elemento en el contenedor padre
       document.getElementById('track-list').appendChild(li)
 
     })
   })
-
 
